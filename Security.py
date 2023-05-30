b19b7e1558b68d23961dd643839b3d8e04058dc4
archis-07 <78965001+archis-07@users.noreply.github.com>
2023-05-30 22:29:46 +0530

    Create Security.py

---
+++ Security.py
@@ -0,0 +1,7 @@
+username = input("enter the username : ")
+password= input("enter the password: ")
+if username == "Anushka" and password == "20":
+    print("login Successfull \n")
+    print("Welcome Anushka ")
+else:
+    print("unvalid username or password.")
